62d1924f3c4c9f82364ad82296548c31bb258b2e
Eric Barefoot <eabarefoot@gmail.com>
2018-08-20 14:54:49 -0500

    add version bump shell script file

---
+++ .version_update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# bump_version.sh
+# this script goes through all files which include coded instances of the `compact proposal` version number and date and replace them.
+# Eric Barefoot
+# Aug 2018
+
+# sed -i -e 's/$1/$2/' <compact_proposal/compact_proposal.cls
+#
